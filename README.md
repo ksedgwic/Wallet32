0b4b7a7d3c4ea087429e8ee48c26418555a00221
Ken Sedgwick <ken@bonsai.com>
2013-10-01 09:14:37 -0700

    Added ActivityBar.

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 AndroidElectrum
 ===============
 
-### Setup in Eclipse
+### Setup
+
+Clone the project source from github:
 
     git clone git@github.com:ksedgwic/AndroidElectrum.git
 
+Import the project into Eclipse
+
     File -> Import ... -> General -> Existing Projects into Workspace
 
-### Add v7 appcompat to Eclipse
+Add v7 appcompat to Eclipse
 
     http://developer.android.com/tools/support-library/setup.html#libs-with-res
-
--- src/com/bonsai/androidelectrum/MainActivity.java
+++ src/com/bonsai/androidelectrum/MainActivity.java
@@ -1,12 +1,13 @@
 package com.bonsai.androidelectrum;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +18,26 @@ public class MainActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		getMenuInflater().inflate(R.menu.main_actions, menu);
+        return super.onCreateOptionsMenu(menu);
 	}
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+        case R.id.action_settings:
+            openSettings();
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    protected void openSettings()
+    {
+        // FIXME - Implement this.
+    }
 
     public void sendBitcoin(View view) {
         Intent intent = new Intent(this, SendBitcoinActivity.class);
