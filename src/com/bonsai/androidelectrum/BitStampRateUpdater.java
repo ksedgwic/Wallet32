ded09c063083d73c6613e5f5d8a03b8297d80943
Ken Sedgwick <ken@bonsai.com>
2013-10-05 14:28:55 -0700

    Working BitStamp rate updater!

--- src/com/bonsai/androidelectrum/BitStampRateUpdater.java
+++ src/com/bonsai/androidelectrum/BitStampRateUpdater.java
@@ -1,5 +1,19 @@
 package com.bonsai.androidelectrum;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.os.Handler;
 import android.os.Message;
 
@@ -13,11 +27,8 @@ public class BitStampRateUpdater extends Thread {
 
     @Override
     public void run() {
-        double rate = 120.00;
-
         while (true) {
-            // FIXME - Determine the real rate here.
-            rate += 0.10;
+            double rate = fetchLatestRate();
 
             // Update the rate in the activity.
             Message msg = mRateUpdateHandler.obtainMessage();
@@ -28,9 +39,45 @@ public class BitStampRateUpdater extends Thread {
             try {
                 Thread.sleep(5000);
             } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
         }
+    }
+
+    protected final String url = "https://www.bitstamp.net/api/ticker/";
+
+    protected double fetchLatestRate() {
+        try {
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            HttpGet httpGet = new HttpGet(url);
+ 
+            HttpResponse httpResponse = httpClient.execute(httpGet);
+            HttpEntity httpEntity = httpResponse.getEntity();
+            InputStream is = httpEntity.getContent();           
+            BufferedReader reader =
+                new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            is.close();
+            String json = sb.toString();
+            JSONObject jObj = new JSONObject(json);
+            double rate = jObj.getDouble("last");
+            return rate;
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+		return 0;
     }
 }
