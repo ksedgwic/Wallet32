8249288bfcc88415dbe369f41833ef4787fb325a
Ken Sedgwick <ken@bonsai.com>
2013-10-17 18:34:53 -0700

    Updated to uber-bitcoinj (includes dependencies).

--- src/com/bonsai/androidelectrum/MainActivity.java
+++ src/com/bonsai/androidelectrum/MainActivity.java
@@ -1,5 +1,11 @@
 package com.bonsai.androidelectrum;
 
+import com.google.bitcoin.core.Address;
+import com.google.bitcoin.core.AddressFormatException;
+import com.google.bitcoin.core.NetworkParameters;
+import com.google.bitcoin.core.WrongNetworkException;
+import com.google.bitcoin.params.MainNetParams;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -9,10 +15,23 @@ import android.view.View;
 
 public class MainActivity extends ActionBarActivity {
 
+	@SuppressWarnings("unused")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+        NetworkParameters params = MainNetParams.get();
+        try {
+			Address addr = new Address(params,
+			                           "15WXpqRvKBpNgcV9XviHANJmqoguQKthdg");
+		} catch (WrongNetworkException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (AddressFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
