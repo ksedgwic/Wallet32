ceedbd8f9221f736b201d8ef5fd6fea182ab35f3
Ken Sedgwick <ken@bonsai.com>
2013-10-24 19:25:27 -0700

    Added WalletApplication. Disabled bitcoinj stuff for the moment.

---
+++ src/com/bonsai/androidelectrum/WalletApplication.java
@@ -0,0 +1,11 @@
+package com.bonsai.androidelectrum;
+
+import android.app.Application;
+
+public class WalletApplication extends Application {
+	@Override
+	public void onCreate()
+	{
+        super.onCreate();
+    }
+}
--- src/com/bonsai/androidelectrum/WalletService.java
+++ src/com/bonsai/androidelectrum/WalletService.java
@@ -1,16 +1,22 @@
 package com.bonsai.androidelectrum;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 
 import com.google.bitcoin.core.Address;
 import com.google.bitcoin.core.AddressFormatException;
+import com.google.bitcoin.core.ECKey;
 import com.google.bitcoin.core.NetworkParameters;
 import com.google.bitcoin.core.WrongNetworkException;
+import com.google.bitcoin.kits.WalletAppKit;
 import com.google.bitcoin.params.MainNetParams;
+import com.google.bitcoin.params.RegTestParams;
 
 public class WalletService extends Service {
+
+    public WalletAppKit mKit;
 
     public WalletService() {
     }
@@ -27,6 +33,41 @@ public class WalletService extends Service {
 
     @Override
     public void onStart(Intent intent, int startId) {
+
+        /*
+        NetworkParameters params = MainNetParams.get();
+
+        String filePrefix = "android-electrum";
+
+        Context context = getApplicationContext();
+
+        mKit = new WalletAppKit(params, context.getFilesDir(), filePrefix) {
+                @Override
+                protected void onSetupCompleted() {
+                    // This is called in a background thread after
+                    // startAndWait is called, as setting up various
+                    // objects can do disk and network IO that may
+                    // cause UI jank/stuttering in wallet apps if it
+                    // were to be done on the main thread.
+                    if (wallet().getKeychainSize() < 1)
+                        wallet().addKey(new ECKey());
+                }
+            };
+
+        if (params == RegTestParams.get()) {
+            // Regression test mode is designed for testing and
+            // development only, so there's no public network for it.
+            // If you pick this mode, you're expected to be running a
+            // local "bitcoind -regtest" instance.
+            mKit.connectToLocalHost();
+        }
+
+        // Download the block chain and wait until it's done.
+        mKit.startAndWait();
+        */
+
+
+        /*
         NetworkParameters params = MainNetParams.get();
         try {
 			Address addr = new Address(params,
@@ -38,6 +79,7 @@ public class WalletService extends Service {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+        */
     }
 
     @Override
