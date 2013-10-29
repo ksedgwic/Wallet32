16c3e8db6cfab7d9db5af7182f14303a09250cd0
Ken Sedgwick <ken@bonsai.com>
2013-10-29 11:59:34 -0700

    Version with explicit private keys (redacted).

--- src/com/bonsai/androidelectrum/WalletService.java
+++ src/com/bonsai/androidelectrum/WalletService.java
@@ -1,5 +1,7 @@
 package com.bonsai.androidelectrum;
 
+import java.math.BigInteger;
+
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -9,8 +11,11 @@ import android.os.IBinder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.bitcoin.core.AddressFormatException;
+import com.google.bitcoin.core.DumpedPrivateKey;
 import com.google.bitcoin.core.ECKey;
 import com.google.bitcoin.core.NetworkParameters;
+import com.google.bitcoin.core.Wallet.BalanceType;
 import com.google.bitcoin.kits.WalletAppKit;
 import com.google.bitcoin.params.MainNetParams;
 import com.google.bitcoin.params.RegTestParams;
@@ -38,27 +43,32 @@ public class WalletService extends Service
 
             mLogger.info("creating new wallet app kit");
 
-            mKit = new WalletAppKit(mParams, context.getFilesDir(), filePrefix)
+            mKit =
+                new WalletAppKit(mParams, context.getFilesDir(), filePrefix)
                 {
                     @Override
                     protected void onSetupCompleted() {
-                        // This is called in a background thread after
-                        // startAndWait is called, as setting up various
-                        // objects can do disk and network IO that may
-                        // cause UI jank/stuttering in wallet apps if it
-                        // were to be done on the main thread.
-                        if (wallet().getKeychainSize() < 1)
-                            wallet().addKey(new ECKey());
+                        
+                        DumpedPrivateKey dpk;
+						try {
+				            mLogger.info("adding keys");
+
+                            // w32_test0
+							dpk = new DumpedPrivateKey(mParams,
+                                                       "xxx");
+	                        wallet().addKey(dpk.getKey());
+                            // w32_test1
+							dpk = new DumpedPrivateKey(mParams,
+                                                       "yyy");
+	                        wallet().addKey(dpk.getKey());
+
+						} catch (AddressFormatException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+                        
                     }
                 };
-
-            if (mParams == RegTestParams.get()) {
-                // Regression test mode is designed for testing and
-                // development only, so there's no public network for it.
-                // If you pick this mode, you're expected to be running a
-                // local "bitcoind -regtest" instance.
-                mKit.connectToLocalHost();
-            }
 
             mLogger.info("waiting for blockchain setup");
 
@@ -66,6 +76,12 @@ public class WalletService extends Service
             mKit.startAndWait();
 
             mLogger.info("blockchain setup finished");
+
+            BigInteger bal0 = mKit.wallet().getBalance(BalanceType.AVAILABLE);
+            BigInteger bal1 = mKit.wallet().getBalance(BalanceType.ESTIMATED);
+
+            mLogger.info("avail balance = " + bal0.toString());
+            mLogger.info("estim balance = " + bal1.toString());
 
             // FIXME - Need to add Progress and Result.
 			return null;
