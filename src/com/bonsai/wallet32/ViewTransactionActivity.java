a6c7da47abaaf13262159a34dfda98bc5e2f0f87
Ken Sedgwick <ken@bonsai.com>
2014-02-12 09:47:23 -0800

    Made transaction view async.

--- src/com/bonsai/wallet32/ViewTransactionActivity.java
+++ src/com/bonsai/wallet32/ViewTransactionActivity.java
@@ -32,6 +32,7 @@ import com.google.bitcoin.core.TransactionOutput;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -46,8 +47,17 @@ public class ViewTransactionActivity extends BaseWalletActivity {
 
     private String mHash;
 
-    private ArrayList<Address>	mInputAddrs;
-    private ArrayList<Address>	mOutputAddrs;
+    private NetworkParameters mParams;
+
+    private Transaction	mTx = null;
+
+    private ArrayList<Address>				mInputAddrs;
+    private ArrayList<HDAddressDescription> mInputDescrs;
+    private ArrayList<Double>				mInputValues;
+
+    private ArrayList<Address>				mOutputAddrs;
+    private ArrayList<HDAddressDescription>	mOutputDescrs;
+    private ArrayList<Double>				mOutputValues;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -69,20 +79,20 @@ public class ViewTransactionActivity extends BaseWalletActivity {
 
 	@Override
     protected void onWalletStateChanged() {
-        NetworkParameters params = mWalletService.getParams();
+        mParams = mWalletService.getParams();
 
         // Find the transaction in the wallet.
-        Transaction tx = mWalletService.getTransaction(mHash);
+        mTx = mWalletService.getTransaction(mHash);
 
         SimpleDateFormat dateFormater =
             new SimpleDateFormat("yyyy-MM-dd kk:mm:ss");
-        String datestr = dateFormater.format(tx.getUpdateTime());
+        String datestr = dateFormater.format(mTx.getUpdateTime());
         {
             TextView tv = (TextView) findViewById(R.id.date);
             tv.setText(datestr);
         }
         
-        TransactionConfidence conf = tx.getConfidence();
+        TransactionConfidence conf = mTx.getConfidence();
         ConfidenceType ct = conf.getConfidenceType();
         String confstr;
         switch (ct) {
@@ -100,140 +110,176 @@ public class ViewTransactionActivity extends BaseWalletActivity {
             tv.setText(confstr);
         }
 
-        // These nbsp string defaults keep the layout sane ...
-        String acctStrDef =
-            "\u2000\u2000\u2000\u2000\u2000\u2000\u2000\u2000\u2000\u2000";
-        String chainCodeDef =
-            "\u2000";
-        String pathDef =
-            "\u2000\u2000\u2000\u2000\u2000\u2000\u2000\u2000\u2000\u2000";
+        // Enumerate the inputs and outputs in another task because
+        // sometimes this is expensive ...
+        //
+        new EnumerateTransputsTask().execute();
+    }
 
-        TableLayout inputsTable =
-            (TableLayout) findViewById(R.id.inputs_table);
-        inputsTable.removeAllViews(); // Clear any existing table content.
-        // addTransputsHeader(inputsTable);
-
-        double totalInputBalance = 0.0;
-
-        mInputAddrs = new ArrayList<Address>();
-
-        // Enumerate inputs.
-        int ndx = 0;
-        for (TransactionInput txIn : tx.getInputs()) {
-            // See if this address is in our HD wallet.
-            HDAddressDescription descr = null;
-            Address addr = null;
-            try {
-                addr = txIn.getFromAddress();
-                descr = mWalletService.findAddress(addr);
-            }
-            catch (ScriptException ex) {
-                // Just leave things blank if we can't determine
-                // an address ...
-            }
-
-            mInputAddrs.add(addr);
-
-            // What is the value of this input?
-            double value = 0.0;
-            TransactionOutput cto = txIn.getConnectedOutput();
-            if (cto != null)
-                value = cto.getValue().doubleValue() / 1e8;
-
-            totalInputBalance += value;
-
-            // We can fill wallet-specific fields if we found the
-            // address in the wallet.
-            String acctStr = acctStrDef;
-            String chainCode = chainCodeDef;
-            String path = pathDef;
-            if (descr != null) {
-                acctStr = descr.hdAccount.getName();
-                chainCode = descr.hdChain.isReceive() ? "R" : "C";
-                path = descr.hdAddress.getPath();
-            }
-
-            String addrStr = "";
-            if (addr != null)
-                addrStr = addr.toString().substring(0, 8) + "...";
-
-            mLogger.info(String.format("input:  %12s %1s %8s %11s %f",
-                                       acctStr, chainCode, path,
-                                       addrStr, value));
-
-            String valStr = String.format("%.05f", value);
-
-            addTransputsRow(R.id.inputs_table, ndx++, inputsTable,
-                            acctStr, chainCode, path, addrStr, valStr);
-        }
-        addTransputsSum(inputsTable, totalInputBalance);
-
-        TableLayout outputsTable =
-            (TableLayout) findViewById(R.id.outputs_table);
-        outputsTable.removeAllViews(); // Clear any existing table content.
-        // addTransputsHeader(outputsTable);
-
-        double totalOutputBalance = 0.0;
-
-        mOutputAddrs = new ArrayList<Address>();
-
-        // Enumerate outputs.
-        ndx = 0;
-        for (TransactionOutput txOut : tx.getOutputs()) {
-            // See if this address is in our HD wallet.
-            HDAddressDescription descr = null;
-            Address addr = null;
-            try {
-                addr = txOut.getScriptPubKey().getToAddress(params);
-                descr = mWalletService.findAddress(addr);
-            }
-            catch (ScriptException ex) {
-                // Just leave things blank if we can't determine
-                // an address ...
-            }
-
-            mOutputAddrs.add(addr);
-
-            // What is the value of this input?
-            double value = txOut.getValue().doubleValue() / 1e8;
-
-            totalOutputBalance += value;
-
-            // We can fill wallet-specific fields if we found the
-            // address in the wallet.
-            String acctStr = acctStrDef;
-            String chainCode = chainCodeDef;
-            String path = pathDef;
-            if (descr != null) {
-                acctStr = descr.hdAccount.getName();
-                chainCode = descr.hdChain.isReceive() ? "R" : "C";
-                path = descr.hdAddress.getPath();
-            }
-
-            String addrStr = "";
-            if (addr != null)
-                addrStr = addr.toString().substring(0, 8) + "...";
-
-            mLogger.info(String.format("output: %12s %1s %8s %11s %f",
-                                       acctStr, chainCode, path,
-                                       addrStr, value));
-            String valStr = String.format("%.05f", value);
-
-            addTransputsRow(R.id.outputs_table, ndx++, outputsTable,
-                            acctStr, chainCode, path, addrStr, valStr);
-        }
-        addTransputsSum(outputsTable, totalOutputBalance);
-
-        double fee = totalInputBalance - totalOutputBalance;
+    private class EnumerateTransputsTask extends AsyncTask<Void, Void, Void> {
+		protected Void doInBackground(Void... arg0)
         {
-            String valStr = String.format("%.05f", fee);
-            TextView tv = (TextView) findViewById(R.id.fee);
-            tv.setText(valStr);
+            mInputAddrs = new ArrayList<Address>();
+            mInputDescrs = new ArrayList<HDAddressDescription>();
+            mInputValues = new ArrayList<Double>();
+
+            // Enumerate inputs.
+            for (TransactionInput txIn : mTx.getInputs()) {
+                // See if this address is in our HD wallet.
+                HDAddressDescription descr = null;
+                Address addr = null;
+                try {
+                    addr = txIn.getFromAddress();
+                    descr = mWalletService.findAddress(addr);
+                }
+                catch (ScriptException ex) {
+                    // Just leave things blank if we can't determine
+                    // an address ...
+                }
+
+                // What is the value of this input?
+                double value = 0.0;
+                TransactionOutput cto = txIn.getConnectedOutput();
+                if (cto != null)
+                    value = cto.getValue().doubleValue() / 1e8;
+
+                mInputAddrs.add(addr);
+                mInputDescrs.add(descr);
+                mInputValues.add(value);
+
+            }
+
+            mOutputAddrs = new ArrayList<Address>();
+            mOutputDescrs = new ArrayList<HDAddressDescription>();
+            mOutputValues = new ArrayList<Double>();
+
+            // Enumerate outputs.
+            for (TransactionOutput txOut : mTx.getOutputs()) {
+                // See if this address is in our HD wallet.
+                HDAddressDescription descr = null;
+                Address addr = null;
+                try {
+                    addr = txOut.getScriptPubKey().getToAddress(mParams);
+                    descr = mWalletService.findAddress(addr);
+                }
+                catch (ScriptException ex) {
+                    // Just leave things blank if we can't determine
+                    // an address ...
+                }
+
+                // What is the value of this input?
+                double value = txOut.getValue().doubleValue() / 1e8;
+
+                mOutputAddrs.add(addr);
+                mOutputDescrs.add(descr);
+                mOutputValues.add(value);
+            }
+			return null;
         }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            // These nbsp string defaults keep the layout sane ...
+            String acctStrDef =
+                "\u2000\u2000\u2000\u2000\u2000\u2000\u2000\u2000\u2000\u2000";
+            String chainCodeDef =
+                "\u2000";
+            String pathDef =
+                "\u2000\u2000\u2000\u2000\u2000\u2000\u2000\u2000\u2000\u2000";
+
+            TableLayout inputsTable =
+                (TableLayout) findViewById(R.id.inputs_table);
+            inputsTable.removeAllViews(); // Clear any existing table content.
+            // addTransputsHeader(inputsTable);
+
+            double totalInputBalance = 0.0;
+
+            for (int ndx = 0; ndx < mInputAddrs.size(); ++ndx) {
+                Address addr = mInputAddrs.get(ndx);
+                HDAddressDescription descr = mInputDescrs.get(ndx);
+                Double value = mInputValues.get(ndx);
+
+                totalInputBalance += value;
+
+                // We can fill wallet-specific fields if we found the
+                // address in the wallet.
+                String acctStr = acctStrDef;
+                String chainCode = chainCodeDef;
+                String path = pathDef;
+                if (descr != null) {
+                    acctStr = descr.hdAccount.getName();
+                    chainCode = descr.hdChain.isReceive() ? "R" : "C";
+                    path = descr.hdAddress.getPath();
+                }
+
+                String addrStr = "";
+                if (addr != null)
+                    addrStr = addr.toString().substring(0, 8) + "...";
+
+                mLogger.info(String.format("input:  %12s %1s %8s %11s %f",
+                                           acctStr, chainCode, path,
+                                           addrStr, value));
+
+                String valStr = String.format("%.05f", value);
+
+                addTransputsRow(R.id.inputs_table, ndx, inputsTable,
+                                acctStr, chainCode, path, addrStr, valStr);
+            }
+            addTransputsSum(inputsTable, totalInputBalance);
+
+            TableLayout outputsTable =
+                (TableLayout) findViewById(R.id.outputs_table);
+            outputsTable.removeAllViews(); // Clear any existing table content.
+            // addTransputsHeader(outputsTable);
+
+            double totalOutputBalance = 0.0;
+
+            for (int ndx = 0; ndx < mOutputAddrs.size(); ++ndx) {
+                Address addr = mOutputAddrs.get(ndx);
+                HDAddressDescription descr = mOutputDescrs.get(ndx);
+                Double value = mOutputValues.get(ndx);
+
+                totalOutputBalance += value;
+
+                // We can fill wallet-specific fields if we found the
+                // address in the wallet.
+                String acctStr = acctStrDef;
+                String chainCode = chainCodeDef;
+                String path = pathDef;
+                if (descr != null) {
+                    acctStr = descr.hdAccount.getName();
+                    chainCode = descr.hdChain.isReceive() ? "R" : "C";
+                    path = descr.hdAddress.getPath();
+                }
+
+                String addrStr = "";
+                if (addr != null)
+                    addrStr = addr.toString().substring(0, 8) + "...";
+
+                mLogger.info(String.format("output: %12s %1s %8s %11s %f",
+                                           acctStr, chainCode, path,
+                                           addrStr, value));
+                String valStr = String.format("%.05f", value);
+
+                addTransputsRow(R.id.outputs_table, ndx, outputsTable,
+                                acctStr, chainCode, path, addrStr, valStr);
+            }
+            addTransputsSum(outputsTable, totalOutputBalance);
+
+            double fee = totalInputBalance - totalOutputBalance;
+            {
+                String valStr = String.format("%.05f", fee);
+                TextView tv = (TextView) findViewById(R.id.fee);
+                tv.setText(valStr);
+            }
         
-        mLogger.info(String.format(" Total Inputs: %f", totalInputBalance));
-        mLogger.info(String.format("Total Outputs: %f", totalOutputBalance));
-        mLogger.info(String.format("   Miners Fee: %f", fee));
+            mLogger.info(String.format(" Total Inputs: %f",
+                                       totalInputBalance));
+            mLogger.info(String.format("Total Outputs: %f",
+                                       totalOutputBalance));
+            mLogger.info(String.format("   Miners Fee: %f",
+                                       fee));
+        }
     }
 
     private void addTransputsHeader(TableLayout table) {
